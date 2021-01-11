f37adde039e3fc0ccfc692d0fec5365f04d46e3d
da-zar <dr.data@gmx.net>
2021-01-11 23:02:38 +0100

    update documentation

--- R/rkt_roc.R
+++ R/rkt_roc.R
@@ -5,7 +5,7 @@
 #' @param prep A \code{rkt_prep} object.
 #' @param method A number specifying the type of ROC estimate. Possible values can be viewed with \code{show_methods()}.
 #' @param x An object of class \code{rkt_roc}.
-#' @param ... Further parameters passed to \code{\link{plot}} and \code{\link{segments}}
+#' @param ... Further parameters passed to \code{\link{plot}} and \code{\link{lines}}
 #'
 #' @return An object of class \code{rkt_roc} for S3 methods.
 #' @export
