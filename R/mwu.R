9c4cb58da7aabc176934c38cc7f836202cc3104e
da-zar <dr.data@gmx.net>
2021-02-24 19:59:47 +0100

    add Mann-Whitney U test

---
+++ R/mwu.R
@@ -0,0 +1,51 @@
+
+mwu.test <- function(prep, alternative = c("two.sided", "less", "greater"), correct = TRUE) {
+  alternative <- match.arg(alternative)
+
+  neg_n <- as.numeric(prep$neg_n)
+  pos_n <- as.numeric(prep$pos_n)
+  n <- neg_n + pos_n
+
+  ties <- data.table(
+    value = c(
+      get_singularities(prep$pos_ecdf),
+      get_singularities(prep$neg_ecdf)
+    ),
+    count = c(
+      round(get_jumps(prep$pos_ecdf) * prep$pos_n),
+      round(get_jumps(prep$neg_ecdf) * prep$neg_n)
+    )
+  )[, .(count = sum(count)), keyby = value]$count
+
+  roc <- rkt_roc(prep)
+  AUC <- auc(roc)
+
+  U <- round(AUC * neg_n * pos_n)
+
+  tie_correction <- sum(ties^3 - ties) / (n * (n-1))
+
+  mu <- neg_n * pos_n / 2
+  sig <- sqrt(neg_n * pos_n * (n + 1 - tie_correction) / 12)
+
+  correction <- ifelse(correct, 0.5, 0)
+
+  p_value <- switch(
+    alternative,
+    "less" = pnorm(U + correction, mu, sig),
+    "greater" = pnorm(U - correction, mu, sig, lower.tail=FALSE),
+    "two.sided" = 2 * min(pnorm(U + correction, mu, sig),
+                          pnorm(U - correction, mu, sig, lower.tail=FALSE))
+  )
+
+  out <- list(
+    method = "Mann–Whitney U test",
+    data.name = deparse1(substitute(prep)),
+    alternative = alternative,
+    statistic = c("U" = U),
+    p.value = as.numeric(p_value)
+  )
+
+  class(out) <- "htest"
+
+  out
+}
